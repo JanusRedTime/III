698340d48809919cc08d63ccccf6bed7cc62d674
Janusred <horaciok300@hotmail.com>
2022-09-10 00:42:17 -0500

    ruta login

--- III/src/app/navbar/login/login.component.ts
+++ III/src/app/navbar/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component  } from '@angular/core';
 
 @Component({
-  selector: 'app-login'
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent {
